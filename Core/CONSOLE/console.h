fb68576f13f43eb5b3c5dfba3c843e3f9b478206
phuoc <phuocly2304@gmail.com>
2024-06-15 21:23:28 +0200

    update console module

--- Core/CONSOLE/console.h
+++ Core/CONSOLE/console.h
@@ -2,14 +2,43 @@
 #define CONSOLE_H
 
 #include <stdarg.h> //for va_list var arg functions
+#include <stdbool.h>
+
+#define RX_BUFFER_SIZE 1024u
+#define TX_BUFFER_SIZE 1024u
+
+typedef struct
+{
+    bool is_dma;
+    uint8_t rx;
+    uint32_t rx_ptr;
+    uint8_t rx_buffer[RX_BUFFER_SIZE];
+    uint8_t tx_buffer[TX_BUFFER_SIZE];
+} CONSOLE_DATA_T;
 
 /**
- * @brief Custom console log function using UART
+ * @brief Custom console log information function using UART
  *
  * @param fmt arguments
  * @param ...
  */
 void log_info(const char* fmt, ...);
+
+/**
+ * @brief Custom console log warning function using UART
+ *
+ * @param fmt arguments
+ * @param ...
+ */
+void log_warning(const char* fmt, ...);
+
+/**
+ * @brief Custom console log error function using UART
+ *
+ * @param fmt arguments
+ * @param ...
+ */
+void log_error(const char* fmt, ...);
 
 void console_task_entry(void);
 
